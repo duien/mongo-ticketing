9104ec86b88438de608e371660b26b195bc283b3
Emily Price <price.emily@gmail.com>
2010-01-18 18:02:38 -0500

    We'll be needing haml

--- config/environment.rb
+++ config/environment.rb
@@ -20,6 +20,7 @@ Rails::Initializer.run do |config|
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
   # config.gem "aws-s3", :lib => "aws/s3"
   config.gem 'mongo_mapper', :version => '~> 0.6.0'
+  config.gem 'haml'
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
