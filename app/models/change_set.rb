8728b502cfb138cd88a841ccd9ec116b8ab46b17
Emily Price <price.emily@gmail.com>
2010-01-19 13:18:45 -0500

    Get list of changed keys for ChangeSet

--- app/models/change_set.rb
+++ app/models/change_set.rb
@@ -2,4 +2,9 @@ class ChangeSet
   include MongoMapper::EmbeddedDocument
   key :what_changed, Hash
   key :changed_at, Time
+
+  def changed_keys
+    what_changed.keys
+  end
+
 end
--- spec/models/change_set_spec.rb
+++ spec/models/change_set_spec.rb
@@ -3,7 +3,18 @@ require 'spec_helper'
 describe ChangeSet do
   before(:each) do
     @valid_attributes = {
-      
+      :what_changed => {
+        'subject' => [ "original subject", "updated subject" ],
+        'description' => [ nil, "added description" ],
+        'comments' => [ [], [1] ]
+      },
+      :timestamp => Time.now
     }
+  end
+
+  it "should list changed keys" do
+    c = ChangeSet.new(@valid_attributes)
+    c.should have(3).changed_keys
+    c.changed_keys.should include( 'subject', 'description', 'comments' )
   end
 end
