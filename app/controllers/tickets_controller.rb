ade1ee26325448e0c0f2325b768459b207f04f0e
Emily Price <price.emily@gmail.com>
2010-01-29 10:51:03 -0500

    New ticket form & controller actions

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -6,4 +6,18 @@ class TicketsController < ApplicationController
   def show
     @ticket = Ticket.find( params[:id] )
   end
+  
+  def new
+    @ticket = Ticket.new(params[:ticket])
+    if request.post?
+      if @ticket.save
+        flash[:notice] = "Ticket created"
+        redirect_to tickets_path
+      else
+        flash[:notice] = "Unable to create ticket"
+        redirect_to new_ticket_path
+      end
+    end
+  end
+  
 end
