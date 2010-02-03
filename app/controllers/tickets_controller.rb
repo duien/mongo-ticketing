79b5223d459cc6fba7010bb02ca22adb8664ec3c
Emily Price <price.emily@gmail.com>
2010-02-03 12:56:51 -0500

    Changed controller to do REST verbs correctly (and added edit/update)

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -8,15 +8,32 @@ class TicketsController < ApplicationController
   end
   
   def new
+    @ticket = Ticket.new
+  end
+  
+  def create
     @ticket = Ticket.new( params[:ticket] )
-    if request.post?
-      if @ticket.save
-        flash[:notice] = "Ticket created"
-        redirect_to @ticket
-      else
-        flash[:error] = "Unable to create ticket"
-        redirect_to new_ticket_path
-      end
+    if @ticket.save
+      flash[:notice] = "Ticket created"
+      redirect_to @ticket
+    else
+      flash[:error] = "Unable to create ticket"
+      redirect_to new_ticket_path
+    end
+  end
+  
+  def edit
+    @ticket = Ticket.find( params[:id] )
+  end
+  
+  def update
+    @ticket = Ticket.find( params[:id] )
+    if @ticket.update_attributes( params[:ticket] )
+      flash[:notice] = "Ticket updated"
+      redirect_to @ticket
+    else
+      flash[:error] = "Unable to update ticket"
+      render :action => 'edit'
     end
   end
   
