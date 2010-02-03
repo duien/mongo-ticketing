5e8c0ab646a32ff774aaac1d01506e417b14b3a5
Emily Price <price.emily@gmail.com>
2010-02-03 12:44:56 -0500

    Force typecasting of status before validation

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -20,6 +20,12 @@ class Ticket
   
   before_update :detect_changes
   before_update :create_change_set
+  before_validation :typecast_status
+  
+  def typecast_status
+    # why the hell do I have to do this?
+    self.status = self.status.to_sym
+  end
 
   def create_change_set
     what_changed = changes
