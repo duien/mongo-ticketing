ee7bcd512b85fc3f842d58dd3a2de1503288295c
Emily Price <price.emily@gmail.com>
2010-05-13 19:59:32 -0400

    Updating mongo version

--- config/environment.rb
+++ config/environment.rb
@@ -19,7 +19,7 @@ Rails::Initializer.run do |config|
   # config.gem "hpricot", :version => '0.6', :source => "http://code.whytheluckystiff.net"
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
   # config.gem "aws-s3", :lib => "aws/s3"
-  config.gem 'mongo_mapper', :version => '~> 0.6.0'
+  config.gem 'mongo_mapper', :version => '~> 0.7.0'
   config.gem 'haml'
 
   # Only load the plugins named here, in the order given (default is alphabetical).
