83c80edf026946a3110c230d0198f56fd9128a20
Emily Price <price.emily@gmail.com>
2010-05-13 20:00:08 -0400

    Specs for ticket_controller

--- spec/controllers/tickets_controller_spec.rb
+++ spec/controllers/tickets_controller_spec.rb
@@ -1,10 +1,36 @@
 require 'spec_helper'
 
 describe TicketsController do
-
-  #Delete this example and add some real ones
-  it "should use TicketsController" do
-    controller.should be_an_instance_of(TicketsController)
+  
+  before do
+    ticket = mock_model(Ticket)
+    Ticket.stub(:all).and_return([ticket])
+  end
+  
+  describe 'index' do
+    it "should set the tickets variable" do
+      get 'index'
+      assigns[:tickets]
+    end
+    
+    it "should list new and open tickets by default" do
+      # pending "figure out these expectation matchers"
+      Ticket.should_receive(:all) do |options|
+        operator, value = options.detect do |key, value| 
+          key.instance_of? SymbolOperator
+        end
+        operator.instance_variable_get(:@operator).should eql('in')
+        operator.instance_variable_get(:@field).should eql(:status)
+        value.should == [:new, :open]
+      end  
+      #.with(hash_including(an_instance_of(SymbolOperator) => [ :new, :open ]))
+      get 'index'
+    end
+    
+    it "should order by most recent update" do
+      Ticket.should_receive(:all).with(hash_including(:order => 'updated_at desc'))
+      get 'index'
+    end
   end
 
 end
