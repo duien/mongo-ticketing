d624f02975eec2a2b87269bcd3b0d9017ba140cc
Emily Price <price.emily@gmail.com>
2010-06-15 13:26:15 -0400

    Move mongo configuration into initalizer

--- config/environment.rb
+++ config/environment.rb
@@ -42,6 +42,3 @@ Rails::Initializer.run do |config|
   # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}')]
   # config.i18n.default_locale = :de
 end
-
-MongoMapper.database = "ticketing-#{Rails.env}"
-ActionView::Helpers::AssetTagHelper.register_stylesheet_expansion :tickets => ["application", "tickets"]
