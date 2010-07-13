c0c5c9bde29db076e43f364f7c0f34498279bf7c
Emily Price <price.emily@gmail.com>
2010-07-13 13:47:43 -0400

    First shot at status bar and styling

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,14 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  
+  def format_time(time)
+    if time >= 1.week.ago
+      time_ago_in_words time
+    elsif time.year == Time.now.year
+      time.strftime('%b %d')
+    else
+      time.strftime('%b %d, %y')
+    end
+  end
+  
 end
