0e5125cba497c3067c3e6f23867294d6c9b50e9a
Emily Price <price.emily@gmail.com>
2010-08-18 15:09:24 -0400

    Tweak user relationship model

--- app/models/user_relationship.rb
+++ app/models/user_relationship.rb
@@ -7,15 +7,10 @@ class UserRelationship
     TYPES
   end
   
-  key :type, :required => true
+  key :type, Symbol, :required => true
   key :user_id, :required => true
   belongs_to :user
   
-  validates_inclusion_of :user, :within => types
-  before_validation :typecast_type
+  validates_inclusion_of :type, :within => types
   
-  def typecast_type
-    # why the hell do I have to do this?
-    self.type = self.type.to_sym
-  end
 end
