368d4eb61d1ee40e0fd6addfe60fe8b2326b4f39
Emily Price <price.emily@gmail.com>
2010-10-10 20:05:43 -0400

    MongoHQ config [heroku]

--- config/initializers/mongo_mapper.rb
+++ config/initializers/mongo_mapper.rb
@@ -1,9 +1,15 @@
-# load db settings
-db_config = YAML::load(File.read(RAILS_ROOT + "/config/database.yml"))
+if ENV['MONGOHQ_URL']
+  MongoMapper.config = {RAILS_ENV => {'uri' => ENV['MONGOHQ_URL']}}
+  MongoMapper.connect(RAILS_ENV)
+else
+  # load db settings
+  db_config = YAML::load(File.read(RAILS_ROOT + "/config/database.yml"))
 
-# connect to the db
-if db_config[Rails.env] && db_config[Rails.env]['adapter'] == 'mongodb'
-  mongo = db_config[Rails.env]
-  MongoMapper.connection = Mongo::Connection.new(mongo['hostname'], mongo['port'] || 27017, :logger => Rails.logger)
-  MongoMapper.database = mongo['database']
+  # connect to the db
+  if db_config[Rails.env] && db_config[Rails.env]['adapter'] == 'mongodb'
+    mongo = db_config[Rails.env]
+    MongoMapper.connection = Mongo::Connection.new(mongo['hostname'], mongo['port'] || 27017, :logger => Rails.logger)
+    MongoMapper.database = mongo['database']
+  end
 end
+
